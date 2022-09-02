20cff9df517067b4c9c400720c060bf371fd6d6f
xu2327 <2327801183@qq.com>
2022-09-02 10:18:11 +0800

    feat: ejs 演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,22 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    isMe: true,
+    blogList: [
+      {
+        id:1,
+        title:'aaa'
+      },
+      {
+        id:2,
+        title:'bbb'
+      },
+      {
+        id:3,
+        title:'ccc'
+      }
+    ]
   })
 })
 
@@ -20,7 +35,7 @@ router.get('/profile/:userName', async (ctx, next) => {
   }
 })
 
-router.get('/loadMore/:userName/:pageIndex', async (ctx,next)=>{
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
   const { userName, pageIndex } = ctx.params;
   ctx.body = {
     title: 'this is loadMore API',
