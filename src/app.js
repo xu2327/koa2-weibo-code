1168cd5c38706f70cc58e51c40f76b88e11ef380
xu2327 <2327801183@qq.com>
2022-09-06 18:22:14 +0800

    feat: 增加 session 配置

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+const koaStatic = require('koa-static')
+
+const { REDIS_CONF } = require('./conf/db')
 
 // ? 引入了一些路由的注册文件 
 const index = require('./routes/index')
@@ -16,14 +21,29 @@ onerror(app) // ? 这是页面上显示报错信息
 // ? 通过AJAX请求，有json或者text数据 可以通过这个解析出来
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json()) // ? 可以把json转变成对象形式
-app.use(logger()) // ? 日志功能
-app.use(require('koa-static')(__dirname + '/public')) // ? 把public目录静态化了 可以直接访问
+// app.use(logger()) // ? 日志功能
+app.use(koaStatic(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
+}))
+
+// session 配置
+app.keys = ['UIsdf_7878#$']
+app.use(session({
+  key: 'weibo.sid', // cookie name 默认是 `koa.sid`
+  prefix: 'weibo:sess:', // redis key 的前缀，默认是：`koa:sess:`
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 1000, // ms
+  },
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
 }))
 
 // logger
